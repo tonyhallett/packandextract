d285f50f1cd46fd9c6e3f70a6970ac984ae60405
Tony Hallett <tonyhallett74@gmail.com>
2017-04-14 12:11:28 +0100

    state before npm install started running scripts

---
+++ index.js
@@ -0,0 +1,69 @@
+console.log("running script");
+var fs = require('file-system');
+var del = require('delete');
+var npm = require('./npm');//*****************
+var gunzip = require('gunzip-maybe')//***************
+var tar = require('tar-stream')//************
+let path = require('path');
+
+
+
+
+
+var noFilter = () => true;
+
+function packAndExtract(packArg, relativeDirectory, filter, deleteGzippedTarball) {
+    if (!filter) {
+        filter = noFilter;
+    }
+    npm.pack("generator-generator", (err, file) => {
+        if (err) {
+            console.log(err.message);
+        } else {
+            var extract = tar.extract()
+            extract.on('entry', function (header, stream, next) {
+                // call next when you are done with this entry
+                var filePath = header.name.substring(8);
+                if (filter(filePath)) {
+                    var writePath = relativeDirectory + filePath;
+                    var dirPath = path.dirname(writePath);
+                    if (!fs.existsSync(dirPath)) {
+                        fs.mkdirSync(dirPath);
+                    }
+                    var writeStream = fs.createWriteStream(writePath, { flags: 'w' });
+                    writeStream.on('error', (error) => {
+                        console.log(error);
+                        next();
+                    });
+                    writeStream.on('open', () => {
+                        stream.pipe(writeStream);
+                        stream.resume();
+                    });
+                    stream.on('end', function () {
+                        next() // ready for next entry
+                    })
+
+                } else {
+                    next();
+                }
+            })
+
+            extract.on('finish', function () {
+                // all entries read
+                if (deleteGzippedTarball) {
+                    del(file, function (err) {
+                        if (err) {
+                            console.log("error deleting the pack");
+                        }
+                    })
+                }
+
+            })
+            var gzippedTarballStream = fs.createReadStream(file);
+            gzippedTarballStream.on('open', () => {
+                gzippedTarballStream.pipe(gunzip()).pipe(extract);
+            })
+        }
+    });
+}
+
